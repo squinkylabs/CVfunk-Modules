170fb49d90258dd3ae2e8af848875a040f692bfd
CV funk <codygeary@users.noreply.github.com>
2024-02-14 08:38:51 +0100

    Add files via upload

---
+++ EnvelopeArray/build/src/EnvelopeArray_backup.cpp.d
@@ -0,0 +1,423 @@
+build/src/EnvelopeArray_backup.cpp.o: src/EnvelopeArray_backup.cpp \
+  src/plugin.hpp /Users/pingo/Desktop/Rack-SDK/include/rack.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/common.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/logger.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/math.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/string.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/system.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/mutex.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/random.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/network.hpp \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/jansson.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/jansson_config.h \
+  /Users/pingo/Desktop/Rack-SDK/include/asset.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/window/Window.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/window/Svg.hpp \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/nanovg.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/nanosvg.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/GL/glew.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/GLFW/glfw3.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/nanovg_gl.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/nanovg_gl_utils.h \
+  /Users/pingo/Desktop/Rack-SDK/include/context.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/audio.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/midi.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/settings.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/color.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/helpers.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/plugin/Model.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/plugin/Plugin.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/MenuOverlay.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/OpaqueWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/Widget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/event.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/weakptr.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/common.hpp \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/blendish.h \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/MenuItem.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/Menu.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/MenuEntry.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/MenuLabel.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/PortWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/common.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/Tooltip.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/Module.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/Param.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/Port.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/Light.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/ParamQuantity.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/Quantity.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/PortInfo.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/LightInfo.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/ParamWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/ModuleLightWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/MultiLightWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/LightWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/TransparentWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/Scene.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/RackScrollWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/ScrollWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/Scrollbar.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/ZoomWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/RackWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/FramebufferWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/RailWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/ModuleWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/history.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/CableWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/Cable.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgPanel.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/SvgWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/componentlibrary.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgKnob.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/Knob.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/TransformWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/CircularShadow.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgSlider.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SliderKnob.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgPort.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgSwitch.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/Switch.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgScrew.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/AudioDisplay.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/LedDisplay.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/TextField.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/SvgButton.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/app/MidiDisplay.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/widget/OpenGlWidget.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/List.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/TooltipOverlay.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/Slider.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/ProgressBar.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/MenuSeparator.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/Label.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/SequentialLayout.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/Button.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/ChoiceButton.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/OptionButton.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/ui/RadioButton.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/ParamHandle.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/engine/Engine.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/plugin/callbacks.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/common.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/simd/functions.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/simd/Vector.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/simd/common.hpp \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse4.2.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse4.1.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/mmx.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-common.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../hedley.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-detect-clang.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-arch.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-features.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-diagnostic.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-math.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-constify.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-align.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../check.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../debug-trap.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/ssse3.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse3.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse2.h \
+  /Users/pingo/Desktop/Rack-SDK/include/simd/sse_mathfun_extension.h \
+  /Users/pingo/Desktop/Rack-SDK/include/simd/sse_mathfun.h \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/window.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/ode.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/minblep.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/fft.hpp \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/pffft.h \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/ringbuffer.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/resampler.hpp \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/speex/speex_resampler.h \
+  /Users/pingo/Desktop/Rack-SDK/dep/include/speex/speexdsp_types.h \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/fir.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/approx.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/midi.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/vumeter.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/filter.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/digital.hpp \
+  /Users/pingo/Desktop/Rack-SDK/include/dsp/convert.hpp
+
+src/plugin.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/rack.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/common.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/logger.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/math.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/string.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/system.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/mutex.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/random.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/network.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/jansson.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/jansson_config.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/asset.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/window/Window.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/window/Svg.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/nanovg.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/nanosvg.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/GL/glew.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/GLFW/glfw3.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/nanovg_gl.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/nanovg_gl_utils.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/context.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/audio.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/midi.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/settings.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/color.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/helpers.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/plugin/Model.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/plugin/Plugin.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/MenuOverlay.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/OpaqueWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/Widget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/event.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/weakptr.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/common.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/blendish.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/MenuItem.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/Menu.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/MenuEntry.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/MenuLabel.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/PortWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/common.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/Tooltip.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/Module.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/Param.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/Port.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/Light.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/ParamQuantity.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/Quantity.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/PortInfo.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/LightInfo.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/ParamWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/ModuleLightWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/MultiLightWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/LightWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/TransparentWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/Scene.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/RackScrollWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/ScrollWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/Scrollbar.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/ZoomWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/RackWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/FramebufferWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/RailWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/ModuleWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/history.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/CableWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/Cable.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgPanel.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/SvgWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/componentlibrary.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgKnob.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/Knob.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/TransformWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/CircularShadow.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgSlider.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SliderKnob.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgPort.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgSwitch.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/Switch.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgScrew.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/AudioDisplay.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/LedDisplay.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/TextField.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/SvgButton.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/app/MidiDisplay.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/widget/OpenGlWidget.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/List.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/TooltipOverlay.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/Slider.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/ProgressBar.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/MenuSeparator.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/Label.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/SequentialLayout.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/Button.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/ChoiceButton.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/OptionButton.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/ui/RadioButton.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/ParamHandle.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/engine/Engine.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/plugin/callbacks.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/common.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/simd/functions.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/simd/Vector.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/simd/common.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse4.2.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse4.1.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/mmx.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-common.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../hedley.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-detect-clang.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-arch.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-features.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-diagnostic.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-math.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-constify.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../simde-align.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../check.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/../debug-trap.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/ssse3.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse3.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/simde/x86/sse2.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/simd/sse_mathfun_extension.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/simd/sse_mathfun.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/window.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/ode.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/minblep.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/fft.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/pffft.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/ringbuffer.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/resampler.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/speex/speex_resampler.h:
+
+/Users/pingo/Desktop/Rack-SDK/dep/include/speex/speexdsp_types.h:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/fir.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/approx.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/midi.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/vumeter.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/filter.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/digital.hpp:
+
+/Users/pingo/Desktop/Rack-SDK/include/dsp/convert.hpp:
